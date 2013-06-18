c3e9e038d336c87f03f74361a3e5a798a4538f69
James Mitchell <jmitch18@gru.edu>
2013-06-18 00:24:34 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PorpitaApp::Application.config.secret_token = 'e7a12d92df73baa273b76dde0bba323437d5a583dd08e0a057b8f71bd7ad5c3c74dc38918ca9b22424473c0d4e7866872b73548cde13c5c26e207f64e22de5ab'
